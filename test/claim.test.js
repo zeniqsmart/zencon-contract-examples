859b7afda6714e6bfec2e3c26be28e77665b0934
Felix Metaverse <felix@zencon.io>
2022-06-05 18:45:31 -0500

    add another Unit-test: cannot claim an NFT that you do not own

--- test/claim.test.js
+++ test/claim.test.js
@@ -4,7 +4,7 @@ const ZenconToken = artifacts.require("ZenconToken");
 const e18 = 1_000_000_000_000_000_000n;
 
 // since a deployed contract cannot be easily updated, it is advised to test contracts with unit-tests
-contract('Claim Test', () => {
+contract('Claim Test', (accounts) => {
 
     it('claim same NFT twice', async function () {
         // sample of a claiming-test to verify that an NFT can be only claimed once
@@ -23,6 +23,24 @@ contract('Claim Test', () => {
             assert.fail('No exception');
         } catch (e) {
             assert.equal(e.reason, 'cannot claim the same NFT twice', 'Wrong exception thrown');
+        }
+    });
+
+    it('cannot claim an NFT that you do not own', async function () {
+        const nftContract = await ZenconNFT.deployed();
+        const tokenContract = await ZenconToken.deployed();
+
+        const amount = 2_000n * e18;
+        const nftId = 1;
+
+        await tokenContract.approve(nftContract.address, amount, {from: accounts[0]});
+        await nftContract.createRewardNFT(amount, {from: accounts[0]});
+
+        try {
+            await nftContract.claimRewardNFT(nftId, {from: accounts[1]});
+            assert.fail('No exception');
+        } catch (e) {
+            assert.equal(e.reason, 'cannot claim an NFT that you do not own', 'Wrong exception thrown');
         }
     });
 });
